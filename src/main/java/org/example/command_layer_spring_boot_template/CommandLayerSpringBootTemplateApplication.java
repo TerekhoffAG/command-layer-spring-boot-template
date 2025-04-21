19a0c4dfb536b0060f9f1eea2c73e1979c639f28
Terekhov <aterekhov@digital-spirit.ru>
2025-04-21 13:30:11 +0400

    [cl-template-0001] - init commit

---
+++ src/main/java/org/example/command_layer_spring_boot_template/CommandLayerSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.example.command_layer_spring_boot_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandLayerSpringBootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandLayerSpringBootTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/command_layer_spring_boot_template/CommandLayerSpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.command_layer_spring_boot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandLayerSpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
